25d08a23f5611d507ea1786df093c758ea48d09d
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-26 16:19:29 -0700

    added scaffolding for router and routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "../src/Components/App/App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from '../src/Containers/App/App';
+import registerServiceWorker from './registerServiceWorker';
 
+/*eslint-disable */
 ReactDOM.render(<App />, document.getElementById("root"));
+/* eslint-enable */
+
 registerServiceWorker();
