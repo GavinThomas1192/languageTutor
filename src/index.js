74acf1327047445288f97bc9301be18ee7ea4b87
Gavin Thomas <gthomas1192@gmail.com>
2018-04-01 22:40:35 -0700

    login persistance

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/Containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
+
+import appCreateStore from './Lib/app-create-store';
+
+const store = appCreateStore();
 /*eslint-disable */
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 /* eslint-enable */
 
 registerServiceWorker();
