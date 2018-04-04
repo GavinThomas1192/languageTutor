a3ab0727f3729d0263298999dafff86473c79493
GavinThomas1192 <gthomas1192@gmail.com>
2018-04-03 18:15:05 -0700

    working super hacky video chat

--- src/Containers/VideoChat/VideoChat.jsx
+++ src/Containers/VideoChat/VideoChat.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {OTSession, OTPublisher, OTStreams, OTSubscriber} from 'opentok-react';
-
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import React from "react";
+import { OTSession, OTPublisher, OTStreams, OTSubscriber } from "opentok-react";
 
 export default class VideoChat extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <div
-                    id="otEmbedContainer"
-                    style={{
-                    width: '800px',
-                    height: '640px'
-                }}></div>
-                <iframe
-                    src="https://tokbox.com/embed/embed/ot-embed.js?embedId=665b1af9-803b-4c0f-9e92-9b4b49f12fba&room=111&iframe=true"
-                    width="800px"
-                    height="640px"
-                    allow="microphone; camera"></iframe>
-                <h1 style={{
-                    color: 'black'
-                }}>hello from video chat</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div
+          id="otEmbedContainer"
+          style={{
+            width: "800px",
+            height: "640px"
+          }}
+        />
+        <iframe
+          src="https://tokbox.com/embed/embed/ot-embed.js?embedId=665b1af9-803b-4c0f-9e92-9b4b49f12fba&room=111&iframe=true"
+          width="800px"
+          height="640px"
+          allow="microphone; camera"
+        />
+        <h1
+          style={{
+            color: "black"
+          }}
+        >
+          hello from video chat
+        </h1>
+      </div>
+    );
+  }
 }
