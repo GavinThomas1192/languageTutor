c0468c08ffa9edbd076379c5c0651590e25bca26
Gavin Thomas <gthomas1192@gmail.com>
2018-04-07 09:52:11 -0700

    added auth to our firebase functions

--- src/Containers/VideoChat2/VideoChat.jsx
+++ src/Containers/VideoChat2/VideoChat.jsx
@@ -8,7 +8,7 @@ import {
   ConnectionStatus
 } from "opentok-react";
 import firebase from "firebase";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import axios from "axios";
 
 import "./VideoChat.css";
@@ -38,10 +38,7 @@ class VideoChat extends React.Component {
         const updatedUser = snapshot.val();
         console.log("DB UPDATED!", updatedUser);
         return updatedUser.uid === this.props.user.account.uid
-          ? this.setState({
-              sessionId: updatedUser.chatRoomKeys.sessionId,
-              token: updatedUser.chatRoomKeys.token
-            })
+          ? this.setState({sessionId: updatedUser.chatRoomKeys.sessionId, token: updatedUser.chatRoomKeys.token})
           : undefined;
       });
   }
@@ -56,29 +53,25 @@ class VideoChat extends React.Component {
         const allUsers = snapshot.val();
 
         console.log("ALLUSERS FROM DATABASE", allUsers);
-        Object.values(allUsers).map(
-          ele =>
-            ele.isTeacher
-              ? this.setState(
-                  {
-                    onlineUsers: [...this.state.onlineUsers, ele]
-                  },
-                  () => {
-                    console.log(
-                      "FINISHED PULLING FULL USER PROFILES",
-                      this.state
-                    );
-                  }
-                )
-              : undefined
-        );
+        Object
+          .values(allUsers)
+          .map(ele => ele.isTeacher
+            ? this.setState({
+              onlineUsers: [
+                ...this.state.onlineUsers,
+                ele
+              ]
+            }, () => {
+              console.log("FINISHED PULLING FULL USER PROFILES", this.state);
+            })
+            : undefined);
       });
   }
   componentDidUpdate() {
     console.log(this.state);
   }
   componentWillUnmount() {
-    this.setState({ token: "", sessionId: "" });
+    this.setState({token: "", sessionId: ""});
     // this.sessionHelper.disconnect();
     firebase
       .database()
@@ -87,135 +80,140 @@ class VideoChat extends React.Component {
   }
 
   handleTeacherHelpRequest = ele => {
-    this.setState({ requestingTeacher: ele });
+    this.setState({requestingTeacher: ele});
+    firebase
+      .auth()
+      .currentUser
+      .getIdToken()
+      .then((userIdToken) => {
 
-    axios.get("http://localhost:3001/api/getTokens").then(data => {
-      console.log(
-        "GOT DATA FROM OUR BACKEND",
-        data,
-        data.data.id,
-        data.data.token
-      );
-      firebase
-        .database()
-        .ref(`users/${this.props.user.account.uid}/chatRoomKeys`)
-        .set({
-          apiKey: `${process.env.REACT_APP_API_KEY}`,
-          sessionId: data.data.id,
-          token: data.data.token
+        // https : //us-central1-language-tutor-a1bdd.cloudfunctions.net/api/api
+        // "http://localhost:3001/api/getTokens"
+        axios
+          .get("https://us-central1-language-tutor-a1bdd.cloudfunctions.net/api/api/getTokens", {
+          headers: {
+            Authorization: `Bearer ${userIdToken}`
+          }
         })
-        .then(() => {
-          firebase
-            .database()
-            .ref(`onlineUsers/${this.state.requestingTeacher.uid}/chatRoomKeys`)
-            .set({
-              apiKey: `${process.env.REACT_APP_API_KEY}`,
-              sessionId: data.data.id,
-              token: data.data.token
-            })
-            .then(() => {
-              this.setState({
-                sessionId: data.data.id,
-                token: data.data.token
+          .then(data => {
+            console.log("GOT DATA FROM OUR BACKEND", data, data.data.id, data.data.token);
+            firebase
+              .database()
+              .ref(`users/${this.props.user.account.uid}/chatRoomKeys`)
+              .set({apiKey: `${process.env.REACT_APP_API_KEY}`, sessionId: data.data.id, token: data.data.token})
+              .then(() => {
+                firebase
+                  .database()
+                  .ref(`onlineUsers/${this.state.requestingTeacher.uid}/chatRoomKeys`)
+                  .set({apiKey: `${process.env.REACT_APP_API_KEY}`, sessionId: data.data.id, token: data.data.token})
+                  .then(() => {
+                    this.setState({sessionId: data.data.id, token: data.data.token});
+                  });
               });
-            });
-        });
-    });
+          });
+      })
+
   };
 
   render() {
     return (
       <div>
         <div>
-          {this.state.onlineUsers.length > 0 ? (
-            <div>
-              <p>Teachers online now!</p>
+          {this.state.onlineUsers.length > 0
+            ? (
+              <div>
+                <p>Teachers online now!</p>
 
-              <ul>
-                {this.state.onlineUsers.map((ele, index) => (
-                  <li
-                    key={index}
-                    onClick={() => this.handleTeacherHelpRequest(ele)}
-                  >
-                    {ele.username}-{ele.nativeLanguage}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : (
-            <ul>No Active Teachers</ul>
-          )}
-          {this.state.requestingTeacher !== "" && this.state.token !== "" ? (
-            <p>
-              Awesome! We are connecting you to{" "}
-              {this.state.requestingTeacher.name}
-            </p>
-          ) : (
-            undefined
-          )}
+                <ul>
+                  {this
+                    .state
+                    .onlineUsers
+                    .map((ele, index) => (
+                      <li key={index} onClick={() => this.handleTeacherHelpRequest(ele)}>
+                        {ele.username}-{ele.nativeLanguage}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            )
+            : (
+              <ul>No Active Teachers</ul>
+            )}
+          {this.state.requestingTeacher !== "" && this.state.token !== ""
+            ? (
+              <p>
+                Awesome! We are connecting you to{" "} {this.state.requestingTeacher.name}
+              </p>
+            )
+            : (undefined)}
         </div>
         <div
           style={{
-            marginLeft: "30em",
-            marginTop: "30em"
-          }}
-        >
-          {this.state.token !== "" ? (
-            <div className="publisherContainer">
-              <div>Session Status: </div>
+          marginLeft: "30em",
+          marginTop: "30em"
+        }}>
+          {this.state.token !== ""
+            ? (
+              <div className="publisherContainer">
+                <div>Session Status:
+                </div>
 
-              <OTSession
-                apiKey={`${process.env.REACT_APP_API_KEY}`}
-                sessionId={this.state.sessionId}
-                token={this.state.token}
-                onError={this.onSessionError}
-                eventHandlers={this.sessionEventHandlers}
-              >
-                <button
-                  onClick={() => {
-                    this.setState({ video: !this.state.video });
-                  }}
-                >
-                  {this.state.video ? "Disable" : "Enable"} Video
-                </button>
-                <button
-                  onClick={() => {
-                    this.setState({ token: "", sessionId: "" });
-                  }}
-                >
-                  disconnect
-                </button>
-                <OTPublisher
-                  properties={{
+                <OTSession
+                  apiKey={`${process.env.REACT_APP_API_KEY}`}
+                  sessionId={this.state.sessionId}
+                  token={this.state.token}
+                  onError={this.onSessionError}
+                  eventHandlers={this.sessionEventHandlers}>
+                  <button
+                    onClick={() => {
+                    this.setState({
+                      video: !this.state.video
+                    });
+                  }}>
+                    {this.state.video
+                      ? "Disable"
+                      : "Enable"}
+                    Video
+                  </button>
+                  <button
+                    onClick={() => {
+                    this.setState({token: "", sessionId: ""});
+                  }}>
+                    disconnect
+                  </button>
+                  <OTPublisher
+                    properties={{
                     publishAudio: this.state.audio,
                     publishVideo: this.state.video,
                     width: 250,
                     height: 250
                   }}
-                  onPublish={this.state.video}
-                  onError={this.onPublishError}
-                  eventHandlers={this.publisherEventHandlers}
-                />
-                <OTStreams>
-                  <OTSubscriber
-                    properties={{ width: 720, height: 720 }}
-                    onSubscribe={this.onSubscribe}
-                    onError={this.onSubscribeError}
-                    eventHandlers={this.subscriberEventHandlers}
-                  />
-                </OTStreams>
-              </OTSession>
-            </div>
-          ) : (
-            <p>No video stream yet</p>
-          )}
+                    onPublish={this.state.video}
+                    onError={this.onPublishError}
+                    eventHandlers={this.publisherEventHandlers}/>
+                  <OTStreams>
+                    <OTSubscriber
+                      properties={{
+                      width: 720,
+                      height: 720
+                    }}
+                      onSubscribe={this.onSubscribe}
+                      onError={this.onSubscribeError}
+                      eventHandlers={this.subscriberEventHandlers}/>
+                  </OTStreams>
+                </OTSession>
+              </div>
+            )
+            : (
+              <p>No video stream yet</p>
+            )}
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({ user: state.user });
+const mapStateToProps = state => ({user: state.user});
 
 const mapDispatchToProps = dispatch => ({
   // getAllActiveTeachers: () => dispatch(getAllActiveTeachers())
