676b6bf104d46d80b7bac2418998b4a8d8b1325e
keith <keith@raincityambience.com>
2018-04-17 20:37:42 -0700

    Add momentjs to format timestamp in chatroom

--- src/Components/ChatRoomMessage/ChatRoomMessage.jsx
+++ src/Components/ChatRoomMessage/ChatRoomMessage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './ChatRoomMessage.css';
+var moment = require('moment-timezone');
 
 const ChatRoomMessage = ({message, uid}) => {
+
   return uid === message.uid ? (
     <li className="chatroom-message-container">
       <p className="chatroom-body f-end"><span className="user-message">{message.body}</span></p>
@@ -10,7 +12,7 @@ const ChatRoomMessage = ({message, uid}) => {
     :
    (
     <li className="chatroom-message-container">
-      <p className="chatroom-author"><span>{message.author}</span></p>
+      <p className="chatroom-author"><span>{message.author}</span> <span>{moment(message.timestamp).format('HH:mm')}</span></p>
       <p className="chatroom-body"><span className="chat-message">{message.body}</span></p>
     </li>
    )
